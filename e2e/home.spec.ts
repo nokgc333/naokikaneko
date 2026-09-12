625fbfe412ca5f3fa3756be8bf1b72530a1eda31
Naoki Kaneko <nokgc333@gmail.com>
2026-09-12 21:19:35 +0900

    test: add Playwright E2E setup

---
+++ e2e/home.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('トップページに見出しが表示される', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.getByRole('heading', { name: 'naokikaneko' })).toBeVisible();
+});
